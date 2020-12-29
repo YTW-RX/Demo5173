893e01b446d5f1615c6d207208bfe4cd7219be23
YTW-RX <1732773103@qq.com>
2020-12-29 13:21:05 +0800

    2020/12/29 13:20 殷涛文更新
    Signed-off-by: YTW-RX <1732773103@qq.com>

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.mapper.UserMapper;
-import com.example.demo.model.User;
+import com.example.demo.entity.User;
 import com.example.demo.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
